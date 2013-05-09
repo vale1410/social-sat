b8cd300b6f2238cc34ec8c023e231cdc51b8d036
vale <vale1410@gmail.com>
2013-05-09 19:29:50 +1000

    stuff

---
+++ condorcet set/search.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p log
+
+for n in {10..30}
+do
+    for m in {7,9,11,13,15,17,19,21,23,25,27,29}
+    do 
+        gringo cond-pref2.lp --const n=$n --const m=$m  | clasp --time-limit=200 -t 3 --stat --configuration=jumpy > \
+            log/r-$n-$m.log
+    done 
+done
