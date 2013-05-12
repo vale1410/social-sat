83e3db85003485c1e67c63ae9679f962d9f1589e
vale <vale1410@gmail.com>
2013-05-12 18:37:57 +1000

    stuff

--- condorcet set/search.sh
+++ condorcet set/search.sh
@@ -1,12 +1,26 @@
-#!/bin/bash
+#!/bin/zsh
+
+source prll.sh                      
+timeout=20000
 
 mkdir -p log
 
-for n in {10..30}
+argument=()
+
+for n in {21,23,25}
 do
-    for m in {7,9,11,13,15,17,19,21,23,25,27,29}
+    for m in {15,17,19,21,23,25,27}
     do 
-        gringo cond-pref2.lp --const n=$n --const m=$m  | clasp --time-limit=200 -t 3 --stat --configuration=jumpy > \
-            log/r-$n-$m.log
+        a=$n' '$m
+        argument+=($a)
     done 
 done
+
+myfn() {
+    x1=$(echo $1 | cut -d' ' -f1)
+    x2=$(echo $1 | cut -d' ' -f2)
+    gringo cond-pref3-2.lp --const n=$x1 --const m=$x2  |  clasp --stat -t 3,split --configuration=chatty --time-limit=$timeout > \
+            log/r-$x1-$x2-$timeout.log
+}
+ 
+prll -c 2 myfn $argument
