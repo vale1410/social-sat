8723ff2708f52238cbba8d39d9943c053857cc55
vale <vale1410@gmail.com>
2013-05-23 01:29:44 +1000

    generate all till size 10

--- sets/make_sets.sh
+++ sets/make_sets.sh
@@ -2,6 +2,10 @@
 
 i=1; 
 
-gringo sets.lp  | clasp 0 | grep contains | while read x; do echo $x \
+rm -fr tmp.lp
+
+gringo sets.lp --const n=$1 | clasp 0 | grep contains | while read x; do echo $x \
                 | sed 's/$/./g' |sed 's/ /./g' \
-                | sed -e s/num/"s$i"/g; i=$((1+$i)) ; done
+                | sed -e s/num/"s$i"/g >> tmp.lp; i=$((1+$i))  ; done
+
+gringo -t tmp.lp superset.lp > $2
