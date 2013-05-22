5b0edd22438bd09949ca262e50c43fd69adfd45b
vale <vale1410@gmail.com>
2013-05-23 01:07:06 +1000

    generate sets of arbitrary size

---
+++ sets/make_sets.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+i=1; 
+
+gringo sets.lp  | clasp 0 | grep element | while read x; do echo $x \
+                | sed 's/$/./g' |sed 's/ /./g' \
+                | sed -e s/num/"s$i"/g; i=$((1+$i)) ; done
